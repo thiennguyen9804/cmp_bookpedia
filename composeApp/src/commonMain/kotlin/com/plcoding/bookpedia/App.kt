5d27f3aaba909a1dde9643e5f06460f212e22287
hayashing <thiennguyen9804@gmail.com>
2025-05-04 16:16:04 +0700

    first commit

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -0,0 +1,30 @@
+package com.plcoding.bookpedia
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.compose_multiplatform
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoot
+import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
+
+@Composable
+@Preview
+fun App() {
+    BookListScreenRoot(
+        viewModel = remember { BookListViewModel() },
+        onBookClick = {
+
+        }
+    )
+}
